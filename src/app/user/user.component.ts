a376d0e16666d0a68a25c60a9cf079d875b6116b
pedronau <pedropefer34@gmail.com>
2025-03-10 08:54:49 +0100

    refactor: volvemos a los decoradores antiguos (explicado en commit antiguo). final video 31

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,23 +9,12 @@ import { Component, computed, Input, input } from '@angular/core';
 })
 export class UserComponent {
   
-  // vamos a usar otro modo de introducir datos de forma dinámica (con señales) para eso importamos "input" y hacemos alguna cosa mas 
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  })
-
-  //lo hemos puesto todo así porque lo hemos pasado todo a señales y hemos de cambiar los metodos de llamada a la clase
-  //como dice que lo de las señales es muy nuevo en el proyecto lo quiere dejar con los @Inputs que son decoradores. hago commit para guardar esto
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {}
 }
