d38f848bf6958e20a81e0960358142e526460b21
pedronau <pedropefer34@gmail.com>
2025-03-10 11:49:33 +0100

    refactor: hacemos el output ahora por señales como hicimos antes con los inputs y, como antes, lo borramos porque en las empresas no se usan señales porque son muy nuevas

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,7 +12,8 @@ export class UserComponent {
   @Input ({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter(); vamos a hacer lo mismo de antes y aprender como se hace esto con señales en vez de con decoradores
+  select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
