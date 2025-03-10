fd80783f12e4e033fe7d2dce41af4b841e810236
pedronau <pedropefer34@gmail.com>
2025-03-10 08:49:56 +0100

    refactor: ahora se usan señales en vez de decoradores @input para seleccionar dinamicamente avatar y usuario. termina video 32 (al final lo vuelve a cambiar a los decoradores porque dice que las señales son muy nuevas)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,12 +9,23 @@ import { Component, Input } from '@angular/core';
 })
 export class UserComponent {
   
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  // vamos a usar otro modo de introducir datos de forma dinámica (con señales) para eso importamos "input" y hacemos alguna cosa mas 
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  })
+
+  //lo hemos puesto todo así porque lo hemos pasado todo a señales y hemos de cambiar los metodos de llamada a la clase
+  //como dice que lo de las señales es muy nuevo en el proyecto lo quiere dejar con los @Inputs que son decoradores. hago commit para guardar esto
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
 
   onSelectUser() {}
 }
