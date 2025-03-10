31838512eae6a2e1cfe6f6f41ddd3787cac5695c
pedronau <pedropefer34@gmail.com>
2025-03-10 19:30:13 +0100

    feat: añadimos módulo nuevo que pinta el nombre del usuario que elegimos (no consigo entender como lo hemos hecho la verdad). final video 37

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, output, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,10 +9,10 @@ import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 })
 export class UserComponent {
   
-  @Input ({required: true}) id!: string;
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
