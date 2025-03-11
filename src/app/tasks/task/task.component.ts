2fbb667e2af543d65bbe2c170dc829326606b2a5
pedronau <pedropefer34@gmail.com>
2025-03-11 11:14:36 +0100

    feat: añade componente dinamico en el template de task para que aparezcan distintas tareas por usuario. añadiendo un input nuevo y en type en el file ts. final video 46.

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+type Task = {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-task',
@@ -7,6 +15,9 @@ import { Component } from '@angular/core';
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
+
 export class TaskComponent {
+
+  @Input({required: true}) task!: Task;
 
 }
