29fef2aa43304e61452644337862f6564db9e0e1
pedronau <pedropefer34@gmail.com>
2025-03-11 09:23:49 +0100

    refactor: añade un for en el app.html para que vaya iterando en la lista y añada los usuarios de forma dinamica. ademas en el user component usa el type para definir el valor en vez de usar 3 inputs. final video 41

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+type User = {
+  id: string;
+  avatar: string;
+  name: string;
+};
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,16 +15,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class UserComponent {
   
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
+
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id); 
+    this.select.emit(this.user.id); 
   }
 }
