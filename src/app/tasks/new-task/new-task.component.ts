eeff7df7e7ce8feaeddd4edc988eba2ff6b81277
pedronau <pedropefer34@gmail.com>
2025-03-12 11:56:41 +0100

    feat: aprendemos el 2way binding y preparamos el camino para aprender a manejar formularios y implementar el boton de añadir tareas. final video 52.

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,20 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
 
   @Output() cancelNewTask = new EventEmitter<boolean>();
+
+  enteredTitle: string = '';
+  enteredSummary: string = '';
+  enteredDate: string = '';
 
   onCancelNewTask() {
     this.cancelNewTask.emit(false);
